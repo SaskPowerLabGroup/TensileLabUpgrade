ea7848a5df14526a5ab3b1460d96350f2db73764
saskpowerlabgroup <ebowes@saskpower.com>
2022-07-19 11:16:47 -0600

    Added Failure Detection and Mqtt Control Functionality

--- BigBlue PID Control/BBwithTouchups-Ethan/BBwithTouchups-Ethan.ino
+++ BigBlue PID Control/BBwithTouchups-Ethan/BBwithTouchups-Ethan.ino
@@ -7,6 +7,11 @@
 
 */
 
+/* Features still needed: 
+ *  Some way to detect failure
+ *  Way to run pid off of the phidget gague mounted on the back
+ */
+
 #include <PID_v1.h>
 #include <Adafruit_ADS1X15.h>
 
@@ -19,6 +24,13 @@ Adafruit_ADS1115 adcOne;
 //#define PIN_DIR 8
 #define PIN_SETPOINT A0
 #define PIN_INPUT A2
+
+//Percentage drop in 0.5s that defines a failure
+double failurePercent = 0.4 
+
+//Timer variables
+unsigned long previousMillis = 0;
+const long interval = 500;
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
@@ -42,32 +54,27 @@ PID myPID(&Input, &Output, &Setpoint,kp,ki,kd,P_ON_M, DIRECT);
 
 void setup()
 {
-  Serial.begin(9600);
-
+   Serial1.begin(115200);
+   
 analogWriteResolution(12);
   analogWrite(DAC0, 2048);
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
   // Initializes ADC and warns if ADC does not connenct
   if (!adcOne.begin(0x48)) {
-    Serial.println("Failed to initialize ADC One.");
+    Serial1.println("Failed to initialize ADC One.");
     while (1);
-    Serial.println("ADC 1 OK");
+    Serial1.println("ADC 1 OK");
   }
 /* if (!adcTwo.begin(0x49)) {
-  Serial.println("Failed to initialize ADC Two.");
+  Serial1.println("Failed to initialize ADC Two.");
   while (1);
-  Serial.println("ADC 2 OK");
+  Serial1.println("ADC 2 OK");
  }
 */
 
   adcOne.setGain(GAIN_ONE);
   adcOne.setDataRate(RATE_ADS1115_128SPS);
-//  adcTwo.setGain(GAIN_ONE);
- 
-  //InputRaw = adcOne.readADC_SingleEnded(0);
-  //Input = map(InputRaw, 20422, 0, 0, 115000);
-  //Input2Raw = adcTwo.readADC_SingleEnded(0);
   Setpoint = 1000;
 
 
@@ -76,9 +83,9 @@ analogWriteResolution(12);
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(1548, 2548);
   myPID.SetSampleTime(10);
-  //Serial.print("Setup Complete");
-  //Serial.println();
-  Serial.println("Setpoint,Input");
+  //Serial1.print("Setup Complete");
+  //Serial1.println();
+  Serial1.println("Setpoint,Input");
 }
 
 void loop()
@@ -94,32 +101,44 @@ void loop()
   if (!JogMode) {
     analogWrite(DAC0, Output);
   }
-  // Serial.print(Output);
-  // Serial.print( ",");
-  Serial.print(Setpoint);
-  Serial.print( ",");
-  Serial.println(Input);
-  //Serial.println(Input2);
 
-  //Serial.println(Pressure1);
+
+  //posts to mqtt every 500 ms also checks for failure
+  unsigned long currentMillis = millis();
+  double forceDifferential
+  
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    forceDifferential = Input
+    Serial1.print(Setpoint);
+    Serial1.print( ",");
+    Serial1.println(Input);
+  }
+
+ if ((Input/forceDifferential) < failurePercent){
+  Serial1.println("Failure Detected Shutting Off Pump");
+  digitalWrite(2, LOW);
+ }
+  
+  
 }
 
 
 void printHelp()
 {
-  Serial.print( "setpoint:");
-  Serial.println( Setpoint );
+  Serial1.print( "setpoint:");
+  Serial1.println( Setpoint );
 
-  Serial.print( "p:");
-  Serial.println( kp );
+  Serial1.print( "p:");
+  Serial1.println( kp );
 
-  Serial.print( "i:");
-  Serial.println( ki );
+  Serial1.print( "i:");
+  Serial1.println( ki );
 
-  Serial.print( "d:");
-  Serial.println( kd );
+  Serial1.print( "d:");
+  Serial1.println( kd );
 
-  Serial.println();
+  Serial1.println();
 }
 
 
@@ -131,7 +150,7 @@ String       logString = "";
 void parseInput()
 {
   String substr;
-  //check_serial_input();
+  //check_Serial1_input();
 
   char first = inputString.charAt(0);
 
@@ -139,51 +158,46 @@ void parseInput()
   {
     case 'p':
       substr = inputString.substring(1);
-      //      pGain = substr.toInt();
-      //      Serial.print("Kp:");
-      //      Serial.println(pGain);
       kp = substr.toDouble();
-      Serial.print("Kp:");
-      Serial.println(kp);
-      //      writetoEEPROM();
+      Serial1.print("Kp:");
+      Serial1.println(kp);
       myPID.SetTunings(kp, ki, kd);
       break;
 
     case 'i':
       substr = inputString.substring(1);
-      //      iGain = substr.toInt();
-      //      Serial.print("Ki:");
-      //      Serial.println(iGain);
       ki = substr.toDouble();
-      Serial.print("Ki:");
-      Serial.println(ki);
-      //      writetoEEPROM();
+      Serial1.print("Ki:");
+      Serial1.println(ki);
       myPID.SetTunings(kp, ki, kd);
       break;
 
     case 'd':
       substr = inputString.substring(1);
       //      dGain = substr.toInt();
-      //      Serial.print("Kd:");
-      //      Serial.println(dGain);
+      //      Serial1.print("Kd:");
+      //      Serial1.println(dGain);
       kd = substr.toDouble();
-      Serial.print("Kd:");
-      Serial.println(kd);
+      Serial1.print("Kd:");
+      Serial1.println(kd);
       //      writetoEEPROM();
       myPID.SetTunings(kp, ki, kd);
       break;
     // Stops the hydraulic pump
     case 's':
+      Serial1.println("Shutting off pump");
       digitalWrite(2, LOW);
       break;
 
     // Enables the hydraulic pump
     case 'g':
+      Serial1.println("Hydraulic pump set to go");
       digitalWrite(2, HIGH);
       break;
 
     // Jogs the piston in the compressive direction
     case 'c':
+      Serial1.println("testing jog in compressive direction");
       myPID.SetMode(MANUAL);
       JogMode = true;
       analogWrite(DAC0, 1548);
@@ -191,6 +205,7 @@ void parseInput()
 
     // Jogs the piston in the tensile direction
     case 't':
+      Serial1.println("Testing Jog tenisle direction");
       myPID.SetMode(MANUAL);
       JogMode = true;
       analogWrite(DAC0, 2400);
@@ -198,6 +213,7 @@ void parseInput()
 
     //holds the pid at current force value
     case 'h':
+      Serial1.println("testing holf");
       Setpoint = Input;
       myPID.SetMode(AUTOMATIC);
       JogMode = false;
@@ -205,6 +221,7 @@ void parseInput()
 
     // Zeros the Force value
     case 'z':
+      Serial1.println("Zero Testing");
       zeroPoint = InputRaw;
       Setpoint = 0;
       break;
@@ -216,28 +233,34 @@ void parseInput()
     case '\n':
       printHelp();
       break;
+    case 'f':
+      substr = inputString.substring(1);
+      Setpoint = substr.toDouble();
+      
+      Serial1.print("Setpoint:");
+      Serial1.println(Setpoint);
+      break;
     default:
-      Setpoint = inputString.toDouble();
-      Serial.print("Setpoint:");
-      Serial.println(Setpoint);
-
+      Serial1.println(inputString);
+      Serial1.println("Error: Shutting off pump");
+      digitalWrite(2, LOW);
   }
   inputString = "";
 }
 
 /*
-  SerialEvent occurs whenever a new data comes in the
-  hardware serial RX.  This routine is run between each
+  Serial1Event occurs whenever a new data comes in the
+  hardware Serial1 RX.  This routine is run between each
   time loop() runs, so using delay inside loop can delay
   response.  Multiple bytes of data may be available.
 */
-void serialEvent()
+void serialEvent1()
 {
-  while (Serial.available())
+  while (Serial1.available())
   {
     // get the new byte:
-    char inChar = (char)Serial.read();
-    //    Serial.print((int)inChar);
+    char inChar = (char)Serial1.read();
+    //    Serial1.print((int)inChar);
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag
